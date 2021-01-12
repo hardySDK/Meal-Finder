2f248881f612f9a41aac2e03dbf305a8ce95738d
sanket <sddesai@dminc.com>
2021-01-12 18:03:52 +0530

    getting data from API  , displaying elements on DOM

--- app.js
+++ app.js
@@ -18,11 +18,30 @@ function searchMeal(e) {
 
 	//check for empty
 	if (term.trim()) {
+		mealsEl.innerHTML = "";
 		fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data);
+				resultHeading.innerHTML = `<h2>The search result for ${term} :</h2>`;
+
+				if (data.meals === null) {
+					resultHeading.innerHTML = `<h2>There are no search results. Please try again !</h2>`;
+				} else {
+					mealsEl.innerHTML = data.meals
+						.map(
+							(meal) => `<div class="meal">
+					<img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+					<div class="meal-info" data-mealID="${meal.idMeal}">
+						<h3>${meal.strMeal}</h3>
+					</div>
+					</div>`
+						)
+						.join("");
+				}
 			});
+		//clear search text
+		search.value = "";
 	} else {
 		alert("Please enter a search term in the input");
 	}
